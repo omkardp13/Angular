63858f05d7a2a6bdb471aa7a23348d84c5b90053
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-05 22:22:15 +0530

    Add files via upload

---
+++ InlineStyle&Templete/src_SeparateCSSWithNoHTML/src/app/Inline/inline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  template: `<h1> Onkar Patil </h1>`,
+  styleUrls: ['./inline.component.css']
+})
+export class InlineComponent {
+
+}
