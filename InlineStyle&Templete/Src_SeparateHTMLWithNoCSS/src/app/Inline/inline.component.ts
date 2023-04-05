dc4ed90bd1af6c3cac5a91d55b42017e54505c4a
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-05 22:10:29 +0530

    Add files via upload

---
+++ InlineStyle&Templete/Src_SeparateHTMLWithNoCSS/src/app/Inline/inline.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  templateUrl: './inline.component.html',
+  styles : [`
+   p {
+       color:green;
+       font-size:40px;
+   }
+ `]
+})
+export class InlineComponent {
+
+}
