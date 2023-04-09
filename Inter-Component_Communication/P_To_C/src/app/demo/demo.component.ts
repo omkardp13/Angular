cf507ffac5ef97f3ee643761a8f5f48ad5b92aa3
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-09 12:24:48 +0530

    Add files via upload

---
+++ Inter-Component_Communication/P_To_C/src/app/demo/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent
+{
+    @Input() public MessageFromParent="";
+
+    @Input('MessageFromParent') public MyName="";
+   
+    @Input() public MyDataArray:string[]=[];
+
+    @Input() public MyDataObj:any={};
+}
