bd127350becad6228a8a35da2d00907afddbaae9
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-29 21:18:49 +0530

    Add files via upload

---
+++ AuthGuard/src_AuthGuard/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login();
+    this.router.navigate(['/dashboard']);
+  }
+
+}
+
