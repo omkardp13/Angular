0857d4c498dac94707e3b8e52204ce4261a6df02
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-10-13 17:20:49 +0530

    Add files via upload

---
+++ ConfigFiles/CustomDirective/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
